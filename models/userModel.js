8b31c58a47eb16ba7f96df7350474b5a96baaab8
kenchen <a330120231@gmail.com>
2021-01-09 00:24:51 -0800

    完成Login 和 Logout 的功能

--- models/userModel.js
+++ models/userModel.js
@@ -1,33 +1,33 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-      trim: true,
+    {
+        name: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        role: {
+            type: Number,
+            default: 0,
+        },
+        cart: {
+            type: Array,
+            default: [],
+        },
     },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-    role: {
-      type: Number,
-      default: 0,
-    },
-    cart: {
-      type: Array,
-      default: [],
-    },
-  },
-  {
-    timestamps: true,
-  }
+    {
+        timestamps: true,
+    }
 );
 
 module.exports = mongoose.model("User", userSchema);
