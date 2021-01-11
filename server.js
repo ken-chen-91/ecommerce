ed538349fe3a2c9964744d1338df29672d4d7049
kenchen <a330120231@gmail.com>
2021-01-10 23:44:56 -0800

    完成 category的 router 和 control, 完成upload files at cloudinary 的功能

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ app.use(
 // Routes
 app.use('/user', require('./routes/userRouter'))
 app.use('/api', require('./routes/categoryRouter'))
+app.use('/api', require('./routes/upload'))
 
 
 // Coonect to mongodb
