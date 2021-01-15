5464620fdb034a561117b9ab0cc5c140e7c4f912
kenchen <a330120231@gmail.com>
2021-01-14 23:12:19 -0800

    完成Products排序的功能

--- controllers/productCtrl.js
+++ controllers/productCtrl.js
@@ -1,43 +1,89 @@
-const Products = require('../models/productModel.js')
+const Products = require('../models/productModel')
+
+// Filter, sorting and paginating
+
+class APIfeatures {
+    constructor(query, queryString){
+        this.query = query;
+        this.queryString = queryString;
+    }
+    filtering(){
+        const queryObj = {...this.queryString} //queryString = req.query
+
+        const excludedFields = ['page', 'sort', 'limit']
+        excludedFields.forEach(el => delete(queryObj[el]))
+
+        let queryStr = JSON.stringify(queryObj)
+        queryStr = queryStr.replace(/\b(gte|gt|lt|lte|regex)\b/g, match => '$' + match)
+
+        //    gte = greater than or equal
+        //    lte = lesser than or equal
+        //    lt = lesser than
+        //    gt = greater than
+        this.query.find(JSON.parse(queryStr))
+
+        return this;
+    }
+
+    sorting(){
+        if(this.queryString.sort){
+            const sortBy = this.queryString.sort.split(',').join(' ')
+            this.query = this.query.sort(sortBy)
+        }else{
+            this.query = this.query.sort('-createdAt')
+        }
+
+        return this;
+    }
+
+    paginating(){
+        const page = this.queryString.page * 1 || 1
+        const limit = this.queryString.limit * 1 || 9
+        const skip = (page - 1) * limit;
+        this.query = this.query.skip(skip).limit(limit)
+        return this;
+    }
+}
 
 const productCtrl = {
-    getProducts: async (req, res) => {
+    getProducts: async(req, res) =>{
         try {
-            const products = await Products.find()
+            const features = new APIfeatures(Products.find(), req.query)
+                .filtering().sorting().paginating()
 
-            res.json(products)
+            const products = await features.query
+
+            res.json({
+                status: 'success',
+                result: products.length,
+                products: products
+            })
+
         } catch (err) {
             return res.status(500).json({msg: err.message})
         }
     },
-
-    createProduct: async (req, res) => {
+    createProduct: async(req, res) =>{
         try {
-            const {product_id, title, price, description, content, images, category} = req.body
-            if (!images) return res.status(400).json({msg: "No image upload"})
+            const {product_id, title, price, description, content, images, category} = req.body;
+            if(!images) return res.status(400).json({msg: "No image upload"})
 
             const product = await Products.findOne({product_id})
-            if (product)
+            if(product)
                 return res.status(400).json({msg: "This product already exists."})
 
             const newProduct = new Products({
-                product_id: product_id,
-                title: title.toLowerCase(),
-                price: price,
-                description: description,
-                content: content,
-                images: images,
-                category: category
+                product_id, title: title.toLowerCase(), price, description, content, images, category
             })
 
             await newProduct.save()
             res.json({msg: "Created a product"})
+
         } catch (err) {
             return res.status(500).json({msg: err.message})
         }
     },
-
-    deleteProduct: async (req, res) => {
+    deleteProduct: async(req, res) =>{
         try {
             await Products.findByIdAndDelete(req.params.id)
             res.json({msg: "Deleted a Product"})
@@ -45,7 +91,6 @@ const productCtrl = {
             return res.status(500).json({msg: err.message})
         }
     },
-
     updateProduct: async(req, res) =>{
         try {
             const {title, price, description, content, images, category} = req.body;
@@ -59,7 +104,8 @@ const productCtrl = {
         } catch (err) {
             return res.status(500).json({msg: err.message})
         }
-    },
+    }
 }
+
 
 module.exports = productCtrl
