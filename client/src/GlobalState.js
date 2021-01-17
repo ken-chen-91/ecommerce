3ef281829b77e78b7ad143b3b776241cb8413415
kenchen <a330120231@gmail.com>
2021-01-17 00:09:14 -0800

    Detail Product

---
+++ client/src/GlobalState.js
@@ -0,0 +1,20 @@
+import React, { createContext, useState } from "react";
+import ProductsAPI from "./api/ProductsAPI";
+
+export const GlobalState = createContext();
+
+export const DataProvider = ({ children }) => {
+    const [token, setToken] = useState(false);
+
+    const state = {
+        token: [token, setToken],
+        ProductsAPI:ProductsAPI()
+    }
+    ProductsAPI();
+
+    return (
+        <GlobalState.Provider value={state}>
+        {children}
+        </GlobalState.Provider>
+    );
+};
---
+++ client/src/components/mainpages/products/Products.js
@@ -0,0 +1,20 @@
+import React, { useContext } from "react";
+import { GlobalState } from "../../../GlobalState";
+import ProductItem from "../utils/productItem/ProductItem";
+
+const Products = () => {
+  const state = useContext(GlobalState);
+  const [products] = state.ProductsAPI.products;
+  console.log(products);
+  return (
+    <div className="products">
+      {
+        products.map(product => {
+          return <ProductItem key={product._id} product={product} />
+        })
+      }
+    </div>
+  );
+};
+
+export default Products;
